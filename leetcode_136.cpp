2083e122840d2198142d39cedbeec970102125cd
Akshat Jain <jakshat726@gmail.com>
2026-09-09 22:35:44 +0530

    Added new files

---
+++ leetcode_136.cpp
@@ -0,0 +1,16 @@
+// Single Number
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int res=nums[0];
+        for(int i = 1; i<nums.size(); i++){
+            res = res ^ nums[i];
+        }
+        return res;
+    }
+};
