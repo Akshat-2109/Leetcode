83f2b5a5d80476d12e006189da8a0b1c99f8da88
Akshat Jain <jakshat726@gmail.com>
2026-09-16 18:57:38 +0530

    Add new files

---
+++ leetcode_240.cpp
@@ -0,0 +1,30 @@
+// Search a 2D Matrix II
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int row = 0;
+        int col = n - 1;
+
+        while (row < m && col >= 0) {
+
+            if (matrix[row][col] == target) {
+                return true;
+            }
+            else if (matrix[row][col] > target) {
+                col--;       
+            }
+            else {
+                row++;      
+            }
+        }
+
+        return false;
+    }
+};
