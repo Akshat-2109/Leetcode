3aea1fdc8e9fe90e7275b88f50981e8b638e1a97
Akshat Jain <jakshat726@gmail.com>
2026-09-22 11:12:54 +0530

    Add new file

---
+++ leetcode_1047.cpp
@@ -0,0 +1,21 @@
+// Remove All Adjacent Duplicates In String
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string st;
+
+        for (char c : s) {
+            if (!st.empty() && st.back() == c) {
+                st.pop_back();
+            } else {
+                st.push_back(c);
+            }
+        }
+
+        return st;
+    }
+};
